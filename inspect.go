5c7bcf39d339271084ac465eec986b6079281772
haibai <hai.bai.cn@gmail.com>
2019-07-20 20:41:31 +0800

    initial commit

---
+++ inspect.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/eminom/gobencode"
+)
+
+var (
+	fVerbose = flag.Bool("v", false, "verbose")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+
+	// log.Printf("%v", bencode.BNodeStringHex)
+	// os.Exit(-1)
+
+	if len(flag.Args()) < 1 {
+		log.Fatal("error: parameter")
+	}
+
+	ib := flag.Args()[0]
+	chunk, err := ioutil.ReadFile(ib)
+	if err != nil {
+		log.Fatalf("cannot read %v", ib)
+	}
+
+	node, left := bencode.Scan(chunk)
+	if len(left) > 0 {
+		log.Printf("warnings: still some bytes left: %v", left)
+	}
+
+	if *fVerbose {
+		bencode.PrintNode(node, node.Cat, 2)
+	}
+
+	if len(flag.Args()) < 2 {
+		os.Exit(0)
+	}
+
+	rawFile := flag.Args()[1]
+	t := bencode.NewTorrent(node.AsMap()["info"].AsMap())
+
+	log.Printf("Start verifying. %v", strings.Repeat("#", 20))
+
+	verified, err := t.VerifyFile(rawFile)
+	if err != nil {
+		log.Fatalf("error verifying: %v", err)
+	}
+
+	log.Printf("verified: %v", verified)
+}
---
+++ scripts/build.cmd
@@ -0,0 +1,2 @@
+go build -o build/inspect.exe inspect.go 
+echo Y|xcopy build\inspect.exe F:\D\Programs\bin
